cba31ce3b9b69d51e52d3f0c5dac9abc1fa35694
Abdelrahman Rashed <abdelrahman.rashed.ali@gmail.com>
2025-06-19 01:48:53 +0300

    awfawg

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,30 +4,33 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin()
+    {
+        return $this->state(function () {
+            return [
+                'name' => env('ADMIN_NAME', 'admin'),
+                'email' => env('ADMIN_EMAIL', 'admin@admin.com'),
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'password')),
+                'email_verified_at' => now(),
+            ];
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ApiCategoryController;
+use \App\Http\Controllers\ApiProductController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
+Route::get('/categories', [ApiCategoryController::class, 'index']);
+Route::get('/products', [ApiProductController::class, 'index']);
+Route::get('/categories/{categoryId}/products', [ApiProductController::class, 'productsByCategory']);
